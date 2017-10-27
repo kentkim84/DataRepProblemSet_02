fe88c8ec405ea52b888ed6dc99019637791e80e0
kentkim84 <kentkim84@gmail.com>
2017-10-27 23:05:07 +0100

    link index page to guess tamplate page

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	//"fmt"
 	"net/http"
 	"log"
 	"html/template"
@@ -10,28 +11,49 @@ type PageVariables struct {
 	HeadTitle string
 	BodyTitle string
 	Response string
-  }
+	Message string
+}
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	// set local variables
 	headTitle := "Guessing Game"
 	bodyTitle := "Guessing Game"
-	response := r.URL.Path[1:]
+
 	// initialise local variables to struct
 	MyPageVariables := PageVariables {
 		HeadTitle : headTitle,
 		BodyTitle : bodyTitle,
-		Response : response,
 	}
 	// parse the template
 	t, err := template.ParseFiles("index.html")
 	if err != nil { // if there is an error
   		log.Print("template parsing error: ", err) // log error message
 	}
-    t.Execute(w, MyPageVariables)
+	t.Execute(w, MyPageVariables)
+	// fmt.Fprintf(w, "Execute indexHandler")
+}
+
+func guessHandler(w http.ResponseWriter, r *http.Request) {
+	// set local variables
+	headTitle := "Guessing Game"
+	message := "Guess a number between 1 and 20"
+
+	// initialise local variables to struct
+	MyPageVariables := PageVariables {
+		HeadTitle : headTitle,
+		Message : message,
+	}
+	// parse the template
+	t, err := template.ParseFiles("guess.tmpl")
+	if err != nil { // if there is an error
+  		log.Print("template parsing error: ", err) // log error message
+	}
+	t.Execute(w, MyPageVariables)
+	// fmt.Fprintf(w, "Execute guessHandler")
 }
 
 func main() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/guess/", guessHandler)
     http.ListenAndServe(":8080", nil)
 }
