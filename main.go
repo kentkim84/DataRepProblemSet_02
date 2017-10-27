0a58333f7a0d60307375551c3e35be198843efca
kentkim84 <kentkim84@gmail.com>
2017-10-28 00:02:55 +0100

    add random number and set cookie

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	//"fmt"
 	"net/http"
 	"log"
 	"html/template"
+	"math/rand"
+	"time"
+	"strconv"
 )
 
 type PageVariables struct {
@@ -12,6 +14,11 @@ type PageVariables struct {
 	BodyTitle string
 	Response string
 	Message string
+}
+
+// generate a random number
+func genRandInt(minNum int, maxNum int) int {
+    return rand.Intn(maxNum) + minNum
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -37,6 +44,17 @@ func guessHandler(w http.ResponseWriter, r *http.Request) {
 	// set local variables
 	headTitle := "Guessing Game"
 	message := "Guess a number between 1 and 20"
+	// set random number
+	maxNum := 20
+	rand.Seed(time.Now().UnixNano())
+	randNum := genRandInt(1, maxNum)
+
+	// set up cookie
+	cookie := http.Cookie{Name: "target", Value: strconv.Itoa(randNum)}
+	_, err := r.Cookie("target")
+	if err == nil { // if there is no error
+		http.SetCookie(w, &cookie)
+	}
 
 	// initialise local variables to struct
 	MyPageVariables := PageVariables {
